80fb0f0789490fa8451f107f831bc53cb9e38bd4
Sahiru <sahiruelvitigala123@gmail.com>
2024-02-13 00:52:22 +0530

    UI Updated and  Claridfai connected

--- app/src/main/java/com/example/ml_image_analyze_app/ScanFragment.java
+++ app/src/main/java/com/example/ml_image_analyze_app/ScanFragment.java
@@ -1,12 +1,11 @@
 package com.example.ml_image_analyze_app;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,28 +20,40 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import org.tensorflow.lite.Interpreter;
+import com.clarifai.channel.ClarifaiChannel;
+import com.clarifai.credentials.ClarifaiCallCredentials;
+import com.clarifai.grpc.api.Concept;
+import com.clarifai.grpc.api.Data;
+import com.clarifai.grpc.api.Image;
+import com.clarifai.grpc.api.Input;
+import com.clarifai.grpc.api.MultiOutputResponse;
+import com.clarifai.grpc.api.PostModelOutputsRequest;
+import com.clarifai.grpc.api.UserAppIDSet;
+import com.clarifai.grpc.api.V2Grpc;
+import com.clarifai.grpc.api.status.StatusCode;
+import com.google.protobuf.ByteString;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.ArrayList;
+
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
-// Import statements
 
 public class ScanFragment extends Fragment {
-    private static final String TAG = "ScanFragment";
+    private static final String TAG = "ScanFragment"; // Define a tag for logging
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT = 768;
+    private TextView mealNameTextView;
 
     private ImageView imageView;
-    private TextView mealNameTextView;
     private ActivityResultLauncher<Void> cameraLauncher;
     private ActivityResultLauncher<String> galleryLauncher;
-    private Interpreter tflite;
-    private List<String> labels;
+    static final String PAT = "f094eaac4c304f83822ea724493dbafd";
+    static final String USER_ID = "clarifai";
+    static final String APP_ID = "main";
+    static final String MODEL_ID = "food-item-recognition";
+    static final String MODEL_VERSION_ID = "1d5fd481e0cf4826aa72ec3ff049e044";
+ 
 
     public ScanFragment() {
         // Required empty public constructor
@@ -53,79 +64,71 @@ public class ScanFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
 
-        // Initialize views
-        imageView = view.findViewById(R.id.iamgecontainer);
+        Button camBtn = view.findViewById(R.id.cambtn);
+        Button galleryBtn = view.findViewById(R.id.gallopnbtn);
         mealNameTextView = view.findViewById(R.id.mealname);
 
-        // Initialize TensorFlow Lite interpreter and model
-        try {
-            initializeTFLiteModel();
-        } catch (IOException e) {
-            Log.e(TAG, "Error initializing TensorFlow Lite model: " + e.getMessage());
-        }
-
-        // Initialize ActivityResultLauncher for camera intent
+        imageView = view.findViewById(R.id.iamgecontainer);
         cameraLauncher = registerForActivityResult(new ActivityResultContracts.TakePicturePreview(), result -> {
-            if (result != null) {
-                imageView.setImageBitmap(result);
-                recognizeDishFromImage(result);
+            if (result != null && result instanceof Bitmap) {
+                Bitmap imageBitmap = (Bitmap) result;
+                imageView.setImageBitmap(imageBitmap);
+                analyzeImage(imageBitmap, mealNameTextView);
+            } else {
+                Log.e(TAG, "Error: Unable to capture image.");
             }
         });
 
-        // Initialize ActivityResultLauncher for gallery intent
         galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
             if (uri != null) {
                 try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), uri);
-                    imageView.setImageBitmap(bitmap);
-                    recognizeDishFromImage(bitmap);
+                    Bitmap originalBitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), uri);
+                    Bitmap resizedBitmap = resizeBitmap(originalBitmap, WIDTH, HEIGHT); // Adjust TARGET_WIDTH and TARGET_HEIGHT as needed
+                    imageView.setImageBitmap(resizedBitmap);
+                    analyzeImage(resizedBitmap, mealNameTextView);
                 } catch (Exception e) {
                     Log.e(TAG, "Error loading image from gallery: " + e.getMessage());
                 }
             }
         });
 
-        // Set click listener for camera button
-        Button camBtn = view.findViewById(R.id.cambtn);
-        camBtn.setOnClickListener(v -> dispatchTakePictureIntent());
 
-        // Set click listener for gallery button
-        Button galleryBtn = view.findViewById(R.id.gallopnbtn);
-        galleryBtn.setOnClickListener(v -> dispatchGalleryIntent());
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST_CODE);
+        } else {
+            setCameraButtonClickListener(camBtn);
+        }
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Gallery button clicked");
+                dispatchGalleryIntent();
+            }
+        });
 
         return view;
     }
 
-    private void initializeTFLiteModel() throws IOException {
-        // Load TensorFlow Lite model from assets folder
-        tflite = new Interpreter(loadModelFile());
-        labels = loadLabels();
+    private void setCameraButtonClickListener(Button camBtn) {
+        camBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Camera button clicked");
+                dispatchTakePictureIntent();
+            }
+        });
     }
-
-    private MappedByteBuffer loadModelFile() throws IOException {
-        FileInputStream inputStream = new FileInputStream("");
-        FileChannel fileChannel = inputStream.getChannel();
-        long startOffset = fileChannel.position();
-        long declaredLength = fileChannel.size();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-    }
-
-    private List<String> loadLabels() throws IOException {
-        // Load labels for image classes
-        List<String> labels = new ArrayList<>();
-        // Load labels from a text file or other source
-        return labels;
-    }
-
-    private void recognizeDishFromImage(Bitmap imageBitmap) {
-
-        ByteBuffer inputBuffer = ;
-        float[][] outputScores = new float[1][labels.size()];
-        tflite.run(inputBuffer, outputScores);
-        // Postprocess the output (get the predicted label)
-        int predictedClassIndex = /* Find the index with highest probability */;
-        String predictedLabel = labels.get(predictedClassIndex);
-        mealNameTextView.setText(predictedLabel);
+    private Bitmap resizeBitmap(Bitmap originalBitmap, int targetWidth, int targetHeight) {
+        float scaleRatio = Math.min(
+                (float) targetWidth / originalBitmap.getWidth(),
+                (float) targetHeight / originalBitmap.getHeight()
+        );
+        int width = Math.round(scaleRatio * originalBitmap.getWidth());
+        int height = Math.round(scaleRatio * originalBitmap.getHeight());
+        return Bitmap.createScaledBitmap(originalBitmap, width, height, false);
     }
 
     private void dispatchTakePictureIntent() {
@@ -136,15 +139,88 @@ public class ScanFragment extends Fragment {
         galleryLauncher.launch("image/*");
     }
 
+    private void analyzeImage(Bitmap bitmap, TextView mealNameTextView) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Resize the image
+                int targetWidth = 800; // Set your desired width
+                int targetHeight = 600; // Set your desired height
+                Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, true);
+
+                // Perform image analysis
+                ByteString byteString = ByteString.copyFrom(Base64.decode(bitmapToBase64(resizedBitmap), Base64.DEFAULT));
+                Image image = Image.newBuilder().setBase64(byteString).build();
+
+                V2Grpc.V2BlockingStub stub = V2Grpc.newBlockingStub(ClarifaiChannel.INSTANCE.getGrpcChannel())
+                        .withCallCredentials(new ClarifaiCallCredentials(PAT));
+
+                MultiOutputResponse postModelOutputsResponse = stub.postModelOutputs(
+                        PostModelOutputsRequest.newBuilder()
+                                .setUserAppId(UserAppIDSet.newBuilder().setUserId(USER_ID).setAppId(APP_ID))
+                                .setModelId(MODEL_ID)
+                                .setVersionId(MODEL_VERSION_ID)
+                                .addInputs(
+                                        Input.newBuilder().setData(
+                                                Data.newBuilder().setImage(image)
+                                        )
+                                )
+                                .build()
+                );
+
+                // Process the response on the UI thread
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        handleModelOutputsResponse(postModelOutputsResponse, mealNameTextView);
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+
+    private void handleModelOutputsResponse(MultiOutputResponse postModelOutputsResponse, TextView mealNameTextView) {
+        if (postModelOutputsResponse.getStatus().getCode() == StatusCode.SUCCESS) {
+            List<Concept> concepts = postModelOutputsResponse.getOutputs(0).getData().getConceptsList();
+            Concept highestConcept = null;
+            double highestValue = Double.MIN_VALUE;
+
+            for (Concept concept : concepts) {
+                Log.d(TAG, "Concept: " + concept.getName() + ", Value: " + concept.getValue());
+
+                if (concept.getValue() > highestValue) {
+                    highestValue = concept.getValue();
+                    highestConcept = concept;
+                }
+            }
+
+            if (highestConcept != null) {
+                String highestConceptName = highestConcept.getName();
+                mealNameTextView.setText(highestConceptName);
+            }
+        } else {
+            Log.e(TAG, "Failed to analyze image. Status: " + postModelOutputsResponse.getStatus());
+        }
+    }
+
+
+    private String bitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Camera permission granted, enable camera functionality
-                // For example, enable the camera button
+                // Camera permission granted, set click listener for camera button
                 Button camBtn = requireView().findViewById(R.id.cambtn);
-                camBtn.setEnabled(true);
+                setCameraButtonClickListener(camBtn);
             }
         }
     }
